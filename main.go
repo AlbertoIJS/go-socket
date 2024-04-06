60e4ad00ec42187dfeaf8ed6d96fbf87bb6d8007
AlbertoIJS <aijs.cdmx@gmail.com>
2024-04-06 05:40:01 -0600

    Rename server to main

--- main.go
+++ main.go
@@ -4,13 +4,14 @@ import (
 	"fmt"
 	"net"
 	"os/exec"
+	"strings"
 )
 
 func main() {
 	// Listen for incoming connections on port 8080
 	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
-		fmt.Println("Error: ", err)
+		fmt.Println("Error listening at port 8080: ", err)
 		return
 	}
 	defer listener.Close()
@@ -19,7 +20,7 @@ func main() {
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Println("Error: ", err)
+			fmt.Println("Error accepting connection: ", err)
 			continue
 		}
 		// Handle the connection in a new goroutine
@@ -39,22 +40,35 @@ func handleConnection(conn net.Conn) {
 	for {
 		n, err := conn.Read(buffer)
 		line := string(buffer[:n])
-		if err != nil {
-			fmt.Println("Error reading data: ", err)
-		}
-		if line == "list remote" {
-			output, err := exec.Command("tree", "remote").Output()
-			if err != nil {
-				fmt.Println("Error executing command: ", err)
-				return
-			}
+		line = strings.TrimSpace(line)
 
-			// Send result to client
-			_, err = conn.Write(output)
-			if err != nil {
-				fmt.Println("Error sending to client: ", err)
-				return
-			}
+		if err != nil {
+			fmt.Println("Error reading data from client: ", err)
+			return
 		}
+
+		res, err := menu(line)
+
+		_, err = conn.Write(res)
+		if err != nil {
+			fmt.Println("Error sending to client: ", err)
+			return
+		}
+	}
+}
+
+func menu(line string) ([]byte, error) {
+	arr := strings.Split(line, " ")
+	command := arr[0]
+
+	switch command {
+	case "list":
+		return exec.Command("ls", "-a").Output()
+	case "mkdir":
+		return exec.Command("mkdir", arr[1]).Output()
+	case "rmdir":
+		return exec.Command("rm -rf", arr[2]).Output()
+	default:
+		return []byte("Unknown command"), nil
 	}
 }
